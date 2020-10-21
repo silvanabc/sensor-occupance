7717a56622b71813e526a0d88ea4ff00d3cd612b
Silvana Barboza <silvana@MacBook-Air-de-Silvana.local>
2020-10-21 11:29:18 +0200

    single page application

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [selectedSensor, setSelectedSensor] = useState(1);
+  const [occupanceSensor, setOccupanceSensor] = useState({});
+
+
+  const showError = () => {
+    alert('Error in retrieving information of Sensor ' + selectedSensor);
+  }
+
+  const showOccupance = () => {
+    //request and set occupance
+    fetch('/api/occupance/' + selectedSensor)
+    .then(res => {
+      if (!res.ok){
+        throw new Error()
+      }
+      return res.json()
+    })
+    .then(
+      result => {
+        if(result.inside != null){
+          setOccupanceSensor({
+            id: selectedSensor,
+            peopleInside: result.inside
+          })
+        }else{
+          showError();
+        }      
+      },
+      error => {
+        showError();
+      }
+    )
+      
+  };
+
+  let handleChange = (e) => {
+    setSelectedSensor(e.target.value)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Meeting Room Occupance</h1>
       </header>
+      
+      <section className="Main-Section">
+        <div>
+          <span>Select a Sensor: </span>
+          <select value={selectedSensor} onChange={handleChange}>
+            <option value="1">Sensor 1</option>
+            <option value="2">Sensor 2</option>
+            <option value="3">Sensor 3</option>
+            <option value="4">Sensor 4</option>
+          </select>
+        </div>
+
+        
+        <button onClick={showOccupance}>Show Occupance</button>
+
+
+        <div className={occupanceSensor.id == null ? 'hidden': ''}>
+          <div className="Sensor-Occupance">
+            Sensor {occupanceSensor.id} reports room occupance of {occupanceSensor.peopleInside} people.
+          </div>
+        </div>
+        
+      </section>
+
     </div>
   );
 }
