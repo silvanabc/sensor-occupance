27c20c06b7a256cc3ed1c3bac955b0dcc463cb87
Silvana Barboza <silvana@MacBook-Air-de-Silvana.local>
2020-10-26 10:18:25 +0100

    react test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen, shallow } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = getByText(/Select a Sensor/i);
+  expect(textElement).toBeInTheDocument();
 });
+
+
+test('renders button', () => {
+  const { container, getByText } = render(<App />);
+  const button = container.querySelector("button");
+  expect(button.innerHTML).toBe("Show Occupance");
+});
+
+
